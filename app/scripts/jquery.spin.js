4ef224c83276a1929f4e0d7831c8426c2166b85a
Erik Waibel <ejwaibel@gmail.com>
2014-09-30 22:35:23 -0700

    Fix issues found with jshint

--- app/scripts/jquery.spin.js
+++ app/scripts/jquery.spin.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * Copyright (c) 2011-2014 Felix Gnass
  * Licensed under the MIT license
@@ -35,18 +36,20 @@ $('#el').spin('flower', 'red');
 
 (function(factory) {
 
-  if (typeof exports == 'object') {
+  if (typeof exports === 'object') {
     // CommonJS
-    factory(require('jquery'), require('spin'))
+    factory(require('jquery'), require('spin'));
   }
-  else if (typeof define == 'function' && define.amd) {
+  else if (typeof define === 'function' && define.amd) {
     // AMD, register as anonymous module
-    define(['jquery', 'spin'], factory)
+    define(['jquery', 'spin'], factory);
   }
   else {
     // Browser globals
-    if (!window.Spinner) throw new Error('Spin.js not present')
-    factory(window.jQuery, window.Spinner)
+    if (!window.Spinner) {
+    	throw new Error('Spin.js not present');
+    }
+    factory(window.jQuery, window.Spinner);
   }
 
 }(function($, Spinner) {
@@ -65,16 +68,16 @@ $('#el').spin('flower', 'red');
         opts = $.extend(
           { color: color || $this.css('color') },
           $.fn.spin.presets[opts] || opts
-        )
-        data.spinner = new Spinner(opts).spin(this)
+        );
+        data.spinner = new Spinner(opts).spin(this);
       }
-    })
-  }
+    });
+  };
 
   $.fn.spin.presets = {
     tiny: { lines: 8, length: 2, width: 2, radius: 3 },
     small: { lines: 8, length: 4, width: 3, radius: 5 },
     large: { lines: 10, length: 8, width: 4, radius: 8 }
-  }
+  };
 
 }));
