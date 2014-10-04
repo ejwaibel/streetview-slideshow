8eb35630a3402fee60d58e7d0a0c240bcce48a7a
Erik Waibel <ejwaibel@gmail.com>
2014-10-03 22:53:03 -0700

    Add 'delay' option to jquery.spin.js

--- app/scripts/jquery.spin.js
+++ app/scripts/jquery.spin.js
@@ -39,44 +39,50 @@ $('#el').spin('flower', 'red');
   if (typeof exports === 'object') {
     // CommonJS
     factory(require('jquery'), require('spin'));
-  }
-  else if (typeof define === 'function' && define.amd) {
+  } else if (typeof define === 'function' && define.amd) {
     // AMD, register as anonymous module
     define(['jquery', 'spin'], factory);
-  }
-  else {
+  } else {
     // Browser globals
     if (!window.Spinner) {
     	throw new Error('Spin.js not present');
     }
+
     factory(window.jQuery, window.Spinner);
   }
 
 }(function($, Spinner) {
 
-  $.fn.spin = function(opts, color) {
-
+  $.fn.spin = function(opts, delay, color) {
     return this.each(function() {
-      var $this = $(this),
-        data = $this.data();
+      var self = this,
+      	$this = $(this),
+        data = $this.data(),
+        wait = delay || 1;
 
       if (data.spinner) {
         data.spinner.stop();
         delete data.spinner;
+      } else if (data.timer) {
+      	clearTimeout(data.timer);
       }
+
       if (opts !== false) {
         opts = $.extend(
           { color: color || $this.css('color') },
           $.fn.spin.presets[opts] || opts
         );
-        data.spinner = new Spinner(opts).spin(this);
+
+        data.timer = setTimeout(function() {
+	        data.spinner = new Spinner(opts).spin(self);
+        }, wait);
       }
     });
   };
 
   $.fn.spin.presets = {
     tiny: { lines: 8, length: 2, width: 2, radius: 3 },
-    small: { lines: 8, length: 4, width: 3, radius: 5 },
+    small: { lines: 8, length: 4, width: 3, radius: 5, fps: 60 },
     large: { lines: 10, length: 8, width: 4, radius: 8 }
   };
 
