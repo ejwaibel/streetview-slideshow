fe497789d7f7f268f47f0d4eefe1d06000664935
Erik Waibel <erik.waibel@gm.com>
2015-06-12 15:28:45 -0700

    Remove url options for 'directions' and 'geocode'

--- app/scripts/leopard.js
+++ app/scripts/leopard.js
@@ -8,13 +8,11 @@
 	var leopard = {
 		api: {
 			key: 'AIzaSyB1jk8Ai169dtl2k6kYatTRiU7ul6gZZd4',
-			directions: '',
-			// geocode: 'https://maps.googleapis.com/maps/api/geocode/json?latlng={{location}}&location_type=ROOFTOP&key={{key}}',
-			streetview: 'https://maps.googleapis.com/maps/api/streetview?size=600x600&location={{location}}{{#heading}}&heading={{heading}}{{/heading}}{{#fov}}&fov={{fov}}{{/fov}}{{#pitch}}&pitch={{pitch}}{{/pitch}}&key={{key}}'
+			streetview: 'https://maps.googleapis.com/maps/api/streetview?size=400x400&location={{location}}{{#heading}}&heading={{heading}}{{/heading}}{{#fov}}&fov={{fov}}{{/fov}}{{#pitch}}&pitch={{pitch}}{{/pitch}}&key={{key}}'
 		},
 		images: {
-			width: 150,
-			height: 150
+			width: 400,
+			height: 175
 		},
 		getRandom: function(from, to) {
 			return Math.random() * (to - from) + from;
@@ -27,11 +25,11 @@
 		getRandomLatLong: function() {
 			var fixed = 3;
 
-		    return {
-		    	// .toFixed() returns string, so ' * 1' is a trick to convert to number
-		    	latitude: (this.getRandom(28.70, 48.85)).toFixed(fixed) * 1,
-		    	longitude: (this.getRandom(-127.50, -55.90)).toFixed(fixed) * 1
-		    };
+			return {
+				// .toFixed() returns string, so ' * 1' is a trick to convert to number
+				latitude: (this.getRandom(28.70, 48.85)).toFixed(fixed) * 1,
+				longitude: (this.getRandom(-127.50, -55.90)).toFixed(fixed) * 1
+			};
 		}
 	};
 
