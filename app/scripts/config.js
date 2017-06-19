814583514b3fe5ede50709ebc72749c0b7fde1d9
Erik Waibel <erik.waibel@gm.com>
2017-06-18 20:17:00 -0700

    Update styles for spinner

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -47,18 +47,18 @@ export const config = {
 	// jscs:disable
 	spinOptions: {
 		lines: 15				// The number of lines to draw
-		, length: 12			// The length of each line
+		, length: 8			// The length of each line
 		, width: 3				// The line thickness
 		, radius: 10			// The radius of the inner circle
 		, scale: 1.25			// Scales overall size of the spinner
 		, corners: .5			// Roundness (0..1)
-		, color: '#008cba'		// #rgb or #rrggbb
+		, color: '#e5dfc5'		// #rgb or #rrggbb
 		, opacity: 1 / 4		// Opacity of the lines
 		, rotate: 0				// Rotation offset
 		, direction: 1			// 1: clockwise, -1: counterclockwise
 		, speed: 1				// Rounds per second
 		, trail: 70				// Afterglow percentage
-		, fps: 20				// Frames per second when using setTimeout()
+		, fps: 60				// Frames per second when using setTimeout()
 		, shadow: true			// Whether to render a shadow
 	},
 	// jscs:enable
